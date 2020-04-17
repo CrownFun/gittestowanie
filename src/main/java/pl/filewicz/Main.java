7ddab34ed6f7aa455a44a37cf55843be679ad710
greg <greg@example.com>
2020-04-17 14:54:33 +0200

    logitech main

--- src/main/java/pl/filewicz/Main.java
+++ src/main/java/pl/filewicz/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         Putin putin = new Putin();
+        Logitech logitech = new Logitech();
+        logitech.hell();
         putin.hello();
     }
 }
