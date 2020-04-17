76a1aa89ca8543ffb9e1396a7c6c74916cb7f814
greg <greg@example.com>
2020-04-17 15:02:23 +0200

    HP hello

---
+++ src/main/java/pl/filewicz/HP.java
@@ -0,0 +1,7 @@
+package pl.filewicz;
+
+public class HP {
+    public void hello(){
+        System.out.println("Hello");
+    }
+}
--- src/main/java/pl/filewicz/Main.java
+++ src/main/java/pl/filewicz/Main.java
@@ -1,11 +1,17 @@
 package pl.filewicz;
 
 public class Main {
+
+    private static int number = 10;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(number);
         Putin putin = new Putin();
         Logitech logitech = new Logitech();
         Dell dell = new Dell();
+        HP hp = new HP();
+        hp.hello();
         dell.dell();
         logitech.hell();
         putin.hello();
