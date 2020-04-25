0e1eba349a369fe16270d89f6a3e784e07b37176
greg <greg@example.com>
2020-04-25 12:53:22 +0200

    usunieto klasa1

--- src/main/java/pl/filewicz/Acer.java
+++ src/main/java/pl/filewicz/Acer.java
@@ -4,6 +4,9 @@ public class Acer {
 
     public  void  hello(){
         System.out.println("hello acer");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 
 }
