dcbb1390ac395a9314a4d98703628208ef451e8f
greg <greg@example.com>
2020-04-25 13:41:20 +0200

    cherry pick

--- src/main/java/pl/filewicz/Sony.java
+++ src/main/java/pl/filewicz/Sony.java
@@ -4,5 +4,6 @@ public class Sony {
     public void hello(){
         System.out.println("hello sony!");
         System.out.println("playstation4");
+        System.out.println("chery pick test");
     }
 }
