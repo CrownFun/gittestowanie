68e6a98a812511996e4d157e74b9692b9aad5067
greg <greg@example.com>
2020-04-25 13:15:37 +0200

    dodaono merode hello

--- src/main/java/pl/filewicz/Acer.java
+++ src/main/java/pl/filewicz/Acer.java
@@ -2,11 +2,8 @@ package pl.filewicz;
 
 public class Acer {
 
-    public  void  hello(){
-        System.out.println("hello acer");
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+    public void hello(){
+        System.out.println("hello Acer");
     }
 
 }
