2a49510a6235002f7262622efeba97372e651428
greg <greg@example.com>
2020-04-25 13:29:52 +0200

    zmiay po stash

--- src/main/java/pl/filewicz/Acer.java
+++ src/main/java/pl/filewicz/Acer.java
@@ -4,9 +4,7 @@ public class Acer {
 
     public  void  hello(){
         System.out.println("hello acer");
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println("hello World");
     }
 
 }
